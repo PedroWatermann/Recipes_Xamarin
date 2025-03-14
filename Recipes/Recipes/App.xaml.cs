0fb8276cecd7fb95e2523cf8f746d6367b164edd
PedroWatermann <pedro.programmer.pt@gmail.com>
2025-03-13 21:55:48 -0300

    Aula 13/03/25

---
+++ Recipes/Recipes/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+using Recipes.Views;
+
+namespace Recipes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new PagPrincipal();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}

8e084d29bb99d91748a963a35bfb34b3401e7348
PedroWatermann <pedro.programmer.pt@gmail.com>
2025-03-18 11:01:06 -0300

    Pull notebook

--- Recipes/Recipes/obj/Debug/netstandard2.0/Recipes.AssemblyInfo.cs
+++ Recipes/Recipes/obj/Debug/netstandard2.0/Recipes.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Recipes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58d9c245881df77287af2da6a5ff58b6d219594f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Recipes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Recipes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
